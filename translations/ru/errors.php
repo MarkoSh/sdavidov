725a337a027625d5285ac218150196d7d14361e0
Mark <markhost@yandex.ru>
2017-03-11 20:04:31 +0300

    Переводы

--- translations/ru/errors.php
+++ translations/ru/errors.php
@@ -1,4 +1,8 @@
 <?php
-	global $_ERRORS;
-	$_ERRORS = array();
+
+global $_ERRORS;
+$_ERRORS = array();
+$_ERRORS['802b207b05fe5cea22e11b9db804b33d'] = 'Ошибка авторизации.';
+$_ERRORS['b51cb75f23091c0f98bea54ae7615080'] = 'Аккаунт с данным электронным адресом не обнаружен';
+
 ?>
