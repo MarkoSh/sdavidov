447b17a1ce028e6e389c27a2fd62b78ad69e2042
Mark <markhost@yandex.ru>
2017-03-11 20:48:40 +0300

    Дренулись плагины

---
+++ themes/default-bootstrap/modules/paypal/translations/ru.php
@@ -0,0 +1,7 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{paypal}default-bootstrap>capture_e8fdd4382b75b42bfbd96eb7b032cf47'] = 'Информация:';
+$_MODULE['<{paypal}default-bootstrap>refund_e8fdd4382b75b42bfbd96eb7b032cf47'] = 'Информация:';
+$_MODULE['<{paypal}default-bootstrap>validation_e8fdd4382b75b42bfbd96eb7b032cf47'] = 'Информация:';
