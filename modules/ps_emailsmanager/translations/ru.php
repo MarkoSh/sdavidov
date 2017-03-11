da382ebaea28daf91f8a2984c760d260cc005643
Mark <markhost@yandex.ru>
2017-03-11 21:23:49 +0300

    Переводы

---
+++ modules/ps_emailsmanager/translations/ru.php
@@ -0,0 +1,8 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{ps_emailsmanager}prestashop>ps_emailsmanager_31fde7b05ac8952dacf4af8a704074ec'] = 'Предпросмотр';
+$_MODULE['<{ps_emailsmanager}prestashop>available_15_31fde7b05ac8952dacf4af8a704074ec'] = 'Предпросмотр';
+$_MODULE['<{ps_emailsmanager}prestashop>available_31fde7b05ac8952dacf4af8a704074ec'] = 'Предпросмотр';
+$_MODULE['<{ps_emailsmanager}prestashop>current_31fde7b05ac8952dacf4af8a704074ec'] = 'Предпросмотр';
