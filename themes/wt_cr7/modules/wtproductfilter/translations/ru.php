7c41ad5a9f5a3d74e3d0c70a3dfc247cbc94a549
Mark <markhost@yandex.ru>
2017-03-24 00:51:27 +0300

    Переводы

---
+++ themes/wt_cr7/modules/wtproductfilter/translations/ru.php
@@ -0,0 +1,11 @@
+<?php
+
+global $_MODULE;
+$_MODULE = array();
+$_MODULE['<{wtproductfilter}wt_cr7>wtproductfilter_e735e23a4b0bede562e50188f5566230'] = 'Показать новые товары';
+$_MODULE['<{wtproductfilter}wt_cr7>wtproductfilter_93cba07454f06a4a960172bbd6e2a435'] = 'Да';
+$_MODULE['<{wtproductfilter}wt_cr7>wtproductfilter_bafd7322c6e97d25b6299b5d6fe8920b'] = 'Нет';
+$_MODULE['<{wtproductfilter}wt_cr7>wtproductfilter_6961066b15998483bd422b6ac3509370'] = 'Интересные товары';
+$_MODULE['<{wtproductfilter}wt_cr7>wtproductfilter_b8e7b14d3ef6e5f3082791623f0a9446'] = 'Специальное предложение';
+$_MODULE['<{wtproductfilter}wt_cr7>wtproductfilter_39c5dfa61825808dbc015048fb878457'] = 'Новые товары';
+$_MODULE['<{wtproductfilter}wt_cr7>wtproductfilter_22c6735b370cecf293e32dca6c678185'] = 'Самые продаваемые';
