115b71114956c1ec3d09f8cd54af53038ae38d2f
Mark <markhost@yandex.ru>
2017-03-23 10:53:10 +0300

    Перевод

--- themes/wt_cr7/modules/wtmegamenu/translations/ru.php
+++ themes/wt_cr7/modules/wtmegamenu/translations/ru.php
@@ -2,4 +2,7 @@
 
 global $_MODULE;
 $_MODULE = array();
+$_MODULE['<{wtmegamenu}wt_cr7>wtmegamenu_93cba07454f06a4a960172bbd6e2a435'] = 'Да';
+$_MODULE['<{wtmegamenu}wt_cr7>wtmegamenu_bafd7322c6e97d25b6299b5d6fe8920b'] = 'Нет';
 $_MODULE['<{wtmegamenu}wt_cr7>wtmegamenu_3adbdb3ac060038aa0e6e6c138ef9873'] = 'Категория';
+$_MODULE['<{wtmegamenu}wt_cr7>wtmegamenu_846495f9ceed11accf8879f555936a7d'] = 'Навигация';
