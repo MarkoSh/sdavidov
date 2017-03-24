e2f01a96952c15e261edc6615bd0ba34a7525b2c
Mark <markhost@yandex.ru>
2017-03-24 21:40:30 +0300

    Переводы

--- themes/wt_cr7/modules/wtcustomhtml/translations/ru.php
+++ themes/wt_cr7/modules/wtcustomhtml/translations/ru.php
@@ -2,15 +2,11 @@
 
 global $_MODULE;
 $_MODULE = array();
-$_MODULE['<{wtcustomhtml}wt_cr7>wtcustomhtml_20015706a8cbd457cbb6ea3e7d5dc9b3'] = 'Настройка обновлена';
 $_MODULE['<{wtcustomhtml}wt_cr7>wtcustomhtml_b78a3223503896721cca1303f776159b'] = 'Название';
 $_MODULE['<{wtcustomhtml}wt_cr7>wtcustomhtml_b9b371458ab7c314f88b81c553f6ce51'] = 'Хук';
-$_MODULE['<{wtcustomhtml}wt_cr7>wtcustomhtml_4c4ad5fca2e7a3f74dbb1ced00381aa4'] = 'HTML';
 $_MODULE['<{wtcustomhtml}wt_cr7>wtcustomhtml_86754577897acfb25deb69039d49d9a7'] = 'Показано';
 $_MODULE['<{wtcustomhtml}wt_cr7>wtcustomhtml_00d23a76e43b46dae9ec7aa9dcbebb32'] = 'Включено';
 $_MODULE['<{wtcustomhtml}wt_cr7>wtcustomhtml_b9f5c797ebbf55adccdd8539a65a0241'] = 'Выключено';
 $_MODULE['<{wtcustomhtml}wt_cr7>wtcustomhtml_c9cc8cce247e49bae79f15173ce97354'] = 'Сохранить';
-$_MODULE['<{wtcustomhtml}wt_cr7>wtcustomhtml_630f6dc397fe74e52d5189e2c80f282b'] = 'Вернуться к списку';
-$_MODULE['<{wtcustomhtml}wt_cr7>list_b9b371458ab7c314f88b81c553f6ce51'] = 'Хук';
 $_MODULE['<{wtcustomhtml}wt_cr7>list_7dce122004969d56ae2e0245cb754d35'] = 'Редактировать';
 $_MODULE['<{wtcustomhtml}wt_cr7>list_f2a6c498fb90ee345d997f888fce3b18'] = 'Удалить';
--- themes/wt_cr7/modules/wtthemeconfigurator/translations/ru.php
+++ themes/wt_cr7/modules/wtthemeconfigurator/translations/ru.php
@@ -7,6 +7,3 @@ $_MODULE['<{wtthemeconfigurator}wt_cr7>setting_theme_7020426cfb0a204051be4b3053d
 $_MODULE['<{wtthemeconfigurator}wt_cr7>setting_theme_cb5feb1b7314637725a2e73bdc9f7295'] = 'Цвет';
 $_MODULE['<{wtthemeconfigurator}wt_cr7>setting_theme_194f5394ae2e9c74dc3c441b92862d1d'] = 'Шрифт';
 $_MODULE['<{wtthemeconfigurator}wt_cr7>setting_theme_526d688f37a86d3c3f27d0c5016eb71d'] = 'Сброс';
-$_MODULE['<{wtthemeconfigurator}wt_cr7>demo_frontend_cb5feb1b7314637725a2e73bdc9f7295'] = 'Цвет';
-$_MODULE['<{wtthemeconfigurator}wt_cr7>demo_frontend_194f5394ae2e9c74dc3c441b92862d1d'] = 'Шрифт';
-$_MODULE['<{wtthemeconfigurator}wt_cr7>demo_frontend_526d688f37a86d3c3f27d0c5016eb71d'] = 'Сброс';
